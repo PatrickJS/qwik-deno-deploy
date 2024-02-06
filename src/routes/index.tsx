eda2874aeb7579b993c6ae3d7a71425f63b22b46
PatrickJS <github@patrickjs.com>
2024-02-05 23:22:19 -0800

    fix: hello world

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,21 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-// import Counter from "~/components/starter/counter/counter";
-// import Hero from "~/components/starter/hero/hero";
-// import Infobox from "~/components/starter/infobox/infobox";
-// import Starter from "~/components/starter/next-steps/next-steps";
+import Counter from "~/components/starter/counter/counter";
+import Hero from "~/components/starter/hero/hero";
+import Infobox from "~/components/starter/infobox/infobox";
+import Starter from "~/components/starter/next-steps/next-steps";
 
 export default component$(() => {
   return (
     <>
+      <Hero />
+
       <div>Hello World lol</div>
+
+      <Counter />
+      <Infobox />
+      <Starter />
     </>
   );
 });
